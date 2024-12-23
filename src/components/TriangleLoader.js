aad8df9d803e2501e1f8cd0deb0b82b90894877f
a.tsagaraev <a.tsagaraev@mtlab.space>
2024-12-23 13:35:30 +0300

    indicator fix

--- src/components/TriangleLoader.js
+++ src/components/TriangleLoader.js
@@ -1,49 +1,15 @@
-import { useState, useEffect } from 'react';
+import './TriangleLoader.css'
 
 const TriangleLoader = () => {
-  const [activePoint, setActivePoint] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setActivePoint((prev) => (prev + 1) % 3);
-    }, 300);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const getOpacity = (pointIndex) => {
-    return pointIndex === activePoint ? 1 : 0.2;
-  };
-
   return (
-    <div className="flex items-center justify-center w-24 h-24">
-      <div className="relative w-12 h-12">
-        {/* Top Star */}
-        <div
-          className="absolute left-1/2 -translate-x-1/2 top-0 text-xl transition-opacity duration-150"
-          style={{ opacity: getOpacity(0) }}
-        >
-          ⁂
-        </div>
-
-        {/* Bottom Right Star */}
-        <div
-          className="absolute bottom-0 right-0 text-xl transition-opacity duration-150"
-          style={{ opacity: getOpacity(1) }}
-        >
-          ⁂
-        </div>
-
-        {/* Bottom Left Star */}
-        <div
-          className="absolute bottom-0 left-0 text-xl transition-opacity duration-150"
-          style={{ opacity: getOpacity(2) }}
-        >
-          ⁂
-        </div>
+    <div className="triangle-loader-container">
+      <div className="triangle-loader">
+        <div className="loader-point top">⁂</div>
+        <div className="loader-point bottom-right">⁂</div>
+        <div className="loader-point bottom-left">⁂</div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TriangleLoader;
+export default TriangleLoader
--- src/data.js
+++ src/data.js
@@ -12,7 +12,8 @@ const objects = [
     channel: 'https://t.me/sashasvoloch',
     model: 'https://efc1ea83-8d42-4e62-ae0e-e88e8e890ca8.selstorage.ru/models/compressed_sleeptable.glb',
     logo: 'https://cdf026bd-e2a5-4a7a-adcd-a11f0029859b.selstorage.ru/svoloch_logo.png',
-    backgroundColor: 'red',
+    backgroundColor: 'white',
+    textColor: 'black',
     aquarium: true,
     position: [-2, -2, 0]
   }
