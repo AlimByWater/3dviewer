4f7db352532b1804ccbcc6a1092c20c4c1181475
doev03 <doev03@gmail.com>
2025-01-10 12:55:38 +0300

    Refactor basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
-const withNextIntl = createNextIntlPlugin("./src/core/i18n/i18n.ts");
+
+const withNextIntl = createNextIntlPlugin('./src/core/i18n/i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/driptech'
+  basePath: process.env.BASE_PATH,
 };
 
 export default withNextIntl(nextConfig);
