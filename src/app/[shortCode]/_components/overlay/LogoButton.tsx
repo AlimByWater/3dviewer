81c4e8a45194130569a2dfb6a994d968ea113f58
a.tsagaraev <a.tsagaraev@mtlab.space>
2025-05-19 14:33:56 +0300

    ref in logo button

--- src/app/[shortCode]/_components/overlay/LogoButton.tsx
+++ src/app/[shortCode]/_components/overlay/LogoButton.tsx
@@ -2,17 +2,30 @@ import TriangleButton from '@/components/TriangleButton';
 import styles from './LogoButton.module.css';
 import SafeArea from '@/components/SafeArea';
 import { useViewer } from '../../_context/ViewerContext';
+import { useLaunchParams } from '@telegram-apps/sdk-react';
 
 const LogoButton = () => {
   const {
-    state: { panelParams },
+    state: { panelParams, slot },
   } = useViewer();
+  const lp = useLaunchParams();
+
+  const userId = slot?.author_id;
+  const slotId = slot?.id;
+
+  let botUrl = process.env.NEXT_PUBLIC_BOT_URL;
+  if (userId) {
+    botUrl += `?start=ref_${userId}`;
+    if (slotId) {
+      botUrl += `_${slotId}`;
+    }
+  }
 
   return (
     <div className={styles.wrapper}>
       <SafeArea>
         <TriangleButton
-          href={process.env.NEXT_PUBLIC_BOT_URL}
+          href={botUrl}
           color={panelParams?.foreground}
         />
       </SafeArea>
